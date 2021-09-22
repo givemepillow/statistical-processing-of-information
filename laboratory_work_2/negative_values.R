39eef498f06c6b942e9d943a917a88ee1143cb55
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-22 23:00:53 +0300

    New version

--- laboratory_work_2/negative_values.R
+++ laboratory_work_2/negative_values.R
@@ -1,15 +1,8 @@
-negative_values <- function(...) {
-  l = list()
-  for (df in list(...)) {
-    nms = names(df)
-    index = 0
-    for (var in df) {
-      index = index + 1
-      if (any(var < 0)) {
-        l[[nms[index]]] = var[var < 0]
-      }
-    }
-    index = 0
-  }
-  return(l)
+negative_values <- function(df) {
+  return(lapply(df[sapply(df, function(col) {
+    if (any(col < 0, na.rm = T)) return(T)
+    return(F)
+  })], function(col) {
+    return(col[col < 0]) 
+  }))
 }
