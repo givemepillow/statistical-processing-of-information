cd2bf065c4e30390b42cdd5fda2a9c9abe9146b2
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-25 13:36:09 +0300

    Updated.

--- laboratory_work_2/column_sum.R
+++ laboratory_work_2/column_sum.R
@@ -1,5 +1,9 @@
-colunm_sum <- function(df) {
-  return(lapply(df, function(col) {
-    return(sum(col[col > 0]))
-  }))
+colunm_sum <- function(...) {
+  l = list()
+  for (d in list(...)) {
+    l = append(l, lapply(d, function(col) {
+      return(sum(col[col > 0]))
+    }))
+  }
+  return(l)
 }
